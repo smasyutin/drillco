11fbded45229e0ef5bf7428024f0375438a282a7
smasyutin <serg.masyutin@gmail.com>
2016-03-05 23:39:38 -0800

    Arguments validation Documenting the project Added start script from npm

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # drillco
+Command line tool to convert drl format into another one.
+
+## Installation
+1. Install nodejs 5.7.1 or later from [nodejs-link]
+2. Clone or download repository code
+3. Inside the repository do `npm install`
+
+Now you are ready to use the tool
+
+[nodejs-link]: https://nodejs.org/en/download/stable/
+
+## Usage
+From command line you can use it like
+
+`npm start -- --source=test/test.drl --output=result.txt --x-offset=20000 --y-offset=65000`
+
+Please note `--` after `nmp start`, this is critical to pass all the other arguments to the tool.
--- drillco.js
+++ drillco.js
@@ -1,39 +1,71 @@
 "use strict";
 
 const argv = require( 'argv' );
-
-const DrlParser = require('./drl_parser.js');
-const OutTranfromer = require('./out_serializer.js');
-
 const options = argv.option([
     {
         name: 'source',
         short: 's',
-        type: 'string,path'
+        type: 'string, path'
     },
     {
-        name: 'output',
+        name: 'target',
         short: 'o',
-        type: 'string,path'
+        type: 'string, path'
     },
     {
         name: 'x-offset',
         short: 'x',
-        type: 'int'
+        type: 'string'
     },
     {
         name: 'y-offset',
         short: 'y',
-        type: 'int'
-    },
+        type: 'string'
+    }
 ]).run();
 
 const fs = require('fs');
 const path = require('path');
 
-const content = fs.readFileSync(path.resolve(__dirname, options.options['source']), 'utf8');
+try {
+    validate(options.options);
 
-const drl = (new DrlParser()).parse(content);
-const out = (new OutTranfromer()).serialize(drl, { x: options.options['x-offset'], y: options.options['y-offset'] });
+    const Parser = require('./drl_parser.js');
+    const drl = (new Parser()).parse(
+        fs.readFileSync(path.resolve(__dirname, options.options['source']), 'utf8')
+    );
 
-fs.writeFileSync(path.resolve(__dirname, options.options['output']), out, 'utf8');
+    const Serializer = require('./out_serializer.js');
+    fs.writeFileSync(path.resolve(__dirname, options.options['target']),
+        (new Serializer()).serialize(drl, {
+            x: parseInt(options.options['x-offset']),
+            y: parseInt(options.options['y-offset'])
+        }),
+        'utf8');
+} catch (e) {
+    console.log(e);
+    process.exit(1);
+}
+
+function validate(options) {
+    fileExists(path.resolve(__dirname, options['source']));
+    isNotEmpty(options['target']);
+    isNumeric(options['x-offset']);
+    isNumeric(options['y-offset']);
+}
+
+function fileExists(file) {
+    fs.accessSync(file, fs.F_OK);
+}
+
+function isNumeric(n) {
+    if(!/^\d+$/.test(n)) {
+        throw new Error(`${n} is not a number`);
+    }
+}
+
+function isNotEmpty(string) {
+    if (!string || string.trim() == '') {
+        throw new Error('target cannot be empty');
+    }
+}
